85d9cda85eb0378acf1bd5b9aa4333187f5ac8f2
Eugen Hildt <eugen.hildt@litlbyte.com>
2024-04-10 15:24:40 +0200

    ui: implemented input-button

---
+++ packages/ui/src/atomics/input-button/InputButton.hooks.ts
@@ -0,0 +1,11 @@
+import { ChangeEvent } from 'react';
+
+import { onChangeProxy } from './InputButton.modal';
+
+export function useChangeEventProxy(callback?: onChangeProxy) {
+  return async (e: ChangeEvent<HTMLInputElement>) => {
+    e?.preventDefault();
+    e?.stopPropagation();
+    await callback?.({ element: e, target: e.target, files: e.target.files });
+  };
+}
