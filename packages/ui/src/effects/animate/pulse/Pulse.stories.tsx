8e2c6ae41715938122cb7a3f90146e1d9301feea
Eugen Hildt <eugen.hildt@litlbyte.com>
2024-04-18 21:30:02 +0200

    ui: moved pulse component

--- packages/ui/src/effects/animate/pulse/Pulse.stories.tsx
+++ packages/ui/src/effects/animate/pulse/Pulse.stories.tsx
@@ -1,7 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
-import { Container } from '@/wireframes/container/Container';
-import { Rotate } from '../transform/rotate/Rotate';
+import { Container } from '@/layouts/container/Container';
+import { Rotate } from '../../transform/rotate/Rotate';
 import { Pulse } from './Pulse';
 
 export default {
