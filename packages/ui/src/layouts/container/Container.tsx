a4787ac4e91261dd8bb568f6fcb7bdb57a153412
Eugen Hildt <eugen.hildt@litlbyte.com>
2024-04-18 21:30:40 +0200

    ui: moved container component

--- packages/ui/src/layouts/container/Container.tsx
+++ packages/ui/src/layouts/container/Container.tsx
@@ -1,6 +1,5 @@
-import { useRef } from 'react';
-
 import classNames from 'classnames';
+import { useCallback, useEffect, useRef } from 'react';
 import { useMouseMove } from './Container.hooks';
 import { ContainerProps } from './Container.modal';
 import style from './Container.module.scss';
@@ -8,13 +7,22 @@ import style from './Container.module.scss';
 export function Container(props: ContainerProps) {
   const ref = useRef<HTMLDivElement>(null);
 
+  const onMouseMoveCallback = useCallback((e: MouseEvent) => {
+    if (ref.current) useMouseMove(ref, e);
+  }, []);
+
+  useEffect(() => {
+    if (ref.current) {
+      ref.current.addEventListener('mousemove', onMouseMoveCallback);
+      return () => ref.current?.removeEventListener('mousemove', onMouseMoveCallback);
+    }
+  }, [onMouseMoveCallback]);
+
   return (
     <div
       ref={ref}
       className={classNames([style.container, { [style.containerHighlight]: props.highlight }])}
       style={props.outerStyle}
-      onMouseMove={(e) => useMouseMove(ref, e)}
-      onClick={props.onClick}
     >
       <div className={style.containerContent} style={props.innerStyle}>
         {props.children}
