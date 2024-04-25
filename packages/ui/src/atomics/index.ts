721e8a6a61fad7bff1ca11cfb5a6aff81d92b65e
Eugen Hildt <eugen.hildt@litlbyte.com>
2024-04-25 16:41:01 +0200

    ui: improved exports

---
+++ packages/ui/src/atomics/index.ts
@@ -0,0 +1,7 @@
+export { Button } from './button/Button';
+export { FileSelector } from './file-selector/FileSelector';
+export { FileInput } from './input/file-input/FileInput';
+export { PageSwitcher } from './page-switcher/PageSwitcher';
+export { SelectBox } from './select-box/SelectBox';
+export { Slider } from './slider/Slider';
+export { Tooltip } from './tooltip/Tooltip';
