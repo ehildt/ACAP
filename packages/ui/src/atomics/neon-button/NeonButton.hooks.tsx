d8776ca2ff602282de321ec471ffd0cc35a2ceba
Eugen Hildt <eugen.hildt@litlbyte.com>
2024-04-25 16:39:07 +0200

    ui: removed NeonButton component

--- packages/ui/src/atomics/neon-button/NeonButton.hooks.tsx
+++
@@ -1,9 +0,0 @@
-import { ProxyFunc } from '../button/Button.modal';
-
-export function useMouseEventProxy(callback?: ProxyFunc) {
-  return (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-    e.preventDefault();
-    e.stopPropagation();
-    callback?.(e.target as HTMLElement, e);
-  };
-}
