c64c16133d618c42d6cc8aee1b91f2a133818903
Eugen Hildt <eugen.hildt@litlbyte.com>
2024-04-25 16:41:01 +0200

    ui: improved exports

---
+++ packages/ui/src/layouts/index.ts
@@ -0,0 +1,5 @@
+export { DesktopLayout } from './frame/desktop/DesktopLayout';
+export { DesktopLayoutFooter } from './frame/desktop/DesktopLayoutFooter';
+export { DesktopLayoutHeader } from './frame/desktop/DesktopLayoutHeader';
+export { DesktopLayoutMain } from './frame/desktop/DesktopLayoutMain';
+export { Line } from './line/Line';
