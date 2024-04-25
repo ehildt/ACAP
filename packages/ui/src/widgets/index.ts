c11ffee688eadeafbdb7237f4b7fb9b532eab1a7
Eugen Hildt <eugen.hildt@litlbyte.com>
2024-04-25 16:41:01 +0200

    ui: improved exports

---
+++ packages/ui/src/widgets/index.ts
@@ -0,0 +1,7 @@
+export { FileImporter } from './file-importer/FileImporter';
+export { useFileImporterImmerStore } from './file-importer/FileImporter.store';
+export { ImageViewer } from './image-viewer/ImageViewer';
+export { JsonViewer } from './json-viewer/JsonViewer';
+export { PdfViewer } from './pdf-viewer/PdfViewer';
+export { Popup } from './popup/Popup';
+export { Scrollbar } from './scrollbar/Scrollbar';
