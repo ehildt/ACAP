11d64bb64c902bf09c40c81ced87c3c5e1e3a073
Eugen Hildt <eugen.hildt@litlbyte.com>
2024-05-16 07:00:45 +0200

    chore: tsconfig & settings

--- apps/frontend/src/widgets/file-importer/FileImporterMenuCard.tsx
+++ apps/frontend/src/widgets/file-importer/FileImporterMenuCard.tsx
@@ -6,22 +6,21 @@ import { useFileImporterImmerStore } from './FileImporter.store';
 
 export function FileImporterMenuCard() {
   const fileSlice = useFileImporterImmerStore();
-  const metadata = useLoadFileContents(fileSlice.files)?.map((f, idx) => {
-    return (
-      <Container key={idx} fadeInOutMS={250 + 100 * idx}>
-        <FileCard
-          filename={f.name}
-          extension={f.extension!}
-          size={f.size}
-          lastModified={f.lastModified}
-          onClick={() => {
-            fileSlice.setShowTreeView(false);
-            fileSlice.setSelectedFile(f);
-          }}
-        />
-      </Container>
-    );
-  });
+  const metadata = useLoadFileContents(fileSlice.files)?.map((f, idx) =>
+    <Container key={idx} fadeInOutMS={100 * idx}>
+      <FileCard
+        filename={f.name}
+        extension={f.extension!}
+        size={f.size}
+        lastModified={f.lastModified}
+        onClick={() => {
+          fileSlice.setShowTreeView(false);
+          fileSlice.setSelectedFile(f);
+        }}
+      />
+    </Container>
+  );
+
 
   return metadata ? (
     <div style={{ display: 'flex', gap: '0.5rem', flexDirection: 'column' }}>{metadata}</div>
