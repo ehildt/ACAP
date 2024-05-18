c25458fa6934bc388405ea3fc523cb25d73ace86
Eugen Hildt <eugen.hildt@litlbyte.com>
2024-05-18 20:18:54 +0200

    chore: init bridge microservice

---
+++ apps/bridge/src/constants/app.constants.ts
@@ -0,0 +1,4 @@
+export const API_DOCS = 'api-docs';
+export const REDIS_PUBSUB = 'REDIS_PUBSUB';
+export const API_DOCS_JSON = 'api-docs-json';
+export const ACAP_REALM_QUEUE = 'ACAP_REALM_QUEUE';
---
+++ apps/bridge/src/validations/app.schema.ts
@@ -0,0 +1,39 @@
+import Joi from 'joi';
+
+import { CONFIG_YML } from '@/configs/config-yml/loader';
+
+export const APP_SCHEMA = {
+  PORT: CONFIG_YML?.appConfig?.port ? Joi.number().default(CONFIG_YML.appConfig.port) : Joi.number().required(),
+
+  ADDRESS: CONFIG_YML?.appConfig?.address
+    ? Joi.string().default(CONFIG_YML.appConfig.address)
+    : Joi.string().required(),
+
+  PRINT_ENV:
+    CONFIG_YML?.appConfig?.printEnv !== undefined
+      ? Joi.boolean().default(CONFIG_YML.appConfig.printEnv)
+      : Joi.boolean().required(),
+
+  START_SWAGGER:
+    CONFIG_YML?.appConfig?.startSwagger !== undefined
+      ? Joi.boolean().default(CONFIG_YML.appConfig.startSwagger)
+      : Joi.boolean().required(),
+
+  NODE_ENV: CONFIG_YML?.appConfig?.nodeEnv
+    ? Joi.string().default(CONFIG_YML.appConfig.nodeEnv)
+    : Joi.string().required(),
+
+  USE_REDIS_PUBSUB:
+    CONFIG_YML?.appConfig?.brokers.useRedisPubSub !== undefined
+      ? Joi.boolean().default(CONFIG_YML.appConfig.brokers.useRedisPubSub)
+      : Joi.boolean().required(),
+
+  USE_BULLMQ:
+    CONFIG_YML?.appConfig?.brokers.useBullMQ !== undefined
+      ? Joi.boolean().default(CONFIG_YML.appConfig.brokers.useBullMQ)
+      : Joi.boolean().required(),
+
+  BODY_LIMIT: CONFIG_YML?.appConfig.bodyLimit
+    ? Joi.number().default(CONFIG_YML.appConfig.bodyLimit)
+    : Joi.number().required(),
+};
