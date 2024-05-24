86a27b6cb9ab3cb620dd3ce76c841306e444e902
Eugen Hildt <eugen.hildt@litlbyte.com>
2024-05-25 01:28:14 +0200

    chore: updated .py scripts and workspaces

--- .python/genpose.sh
+++ .python/genpose.sh
@@ -4,10 +4,10 @@ COMBINED_ENV_FILE=$(mktemp)
 cat ./apps/ms-bridge/env/.env ./apps/backend/env/.env >"$COMBINED_ENV_FILE"
 
 if [[ "$1" == "--up" ]]; then
-    npx dotenv -e "$COMBINED_ENV_FILE" -- python3 .python/genpose.py ./docker-compose.yml --template-dir .templates
+    npx dotenv -e "$COMBINED_ENV_FILE" -- python3 .python/genpose.py ./docker-compose.yml --template-dir .python/templates
     docker compose up
 else
-    npx dotenv -e "$COMBINED_ENV_FILE" -- python3 .python/genpose.py ./docker-compose.yml --template-dir .templates "$@"
+    npx dotenv -e "$COMBINED_ENV_FILE" -- python3 .python/genpose.py ./docker-compose.yml --template-dir .python/templates "$@"
 fi
 
 rm "$COMBINED_ENV_FILE"
